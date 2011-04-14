b8481072dac3ba620798bcec5d5a4d1cf49176fa
Jamie Kirkpatrick <jkp@kirkconsulting.co.uk>
2011-04-14 09:35:14 +0200

    fixed: we were setting the wrong type, thereby screwing up the details view, and probably more (my fault!!!)

--- plex/FileSystem/PlexDirectory.cpp
+++ plex/FileSystem/PlexDirectory.cpp
@@ -1475,9 +1475,12 @@ void CPlexDirectory::Parse(const CURL& url, TiXmlElement* root, CFileItemList &i
     {
       // Get the type.
       const char* pType = element->Attribute("type");
+      string type;
+      
       if (pType)
       {
-        string type = pType;
+        type = pType;
+        
         if (type == "show")
           type = "tvshows";
         else if (type == "season")
@@ -1502,20 +1505,20 @@ void CPlexDirectory::Parse(const CURL& url, TiXmlElement* root, CFileItemList &i
       }
       
       CFileItemPtr item = mediaNode->BuildFileItem(url, *element, isLocal);
-      if (!item)
-        continue;
-      
-      // Set the content type for the item.
-      if (pType)
-        item->SetProperty("mediaType", pType);
-      
-      // Tags.
-      ParseTags(element, item, "Genre");
-      ParseTags(element, item, "Writer");
-      ParseTags(element, item, "Director");
-      ParseTags(element, item, "Role");
-    
-      items.Add(item);
+      if (item)
+      {
+        // Set the content type for the item.
+        if (!type.empty())
+          item->SetProperty("mediaType", type);
+        
+        // Tags.
+        ParseTags(element, item, "Genre");
+        ParseTags(element, item, "Writer");
+        ParseTags(element, item, "Director");
+        ParseTags(element, item, "Role");
+        
+        items.Add(item);
+      }
     }
   }
   
