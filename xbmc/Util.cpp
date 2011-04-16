62f7ee40faa16f3fe38193e48f08a7b3801ba8ac
Jamie Kirkpatrick <jkp@kirkconsulting.co.uk>
2011-04-16 23:21:27 +0200

    fixed: url qualification utility when there are url query parameters present in either string

--- xbmc/Util.cpp
+++ xbmc/Util.cpp
@@ -614,6 +614,16 @@ void CUtil::GetQualifiedFilename(const CStdString &strBasePath, CStdString &strF
   //Make sure you have a full path in the filename, otherwise adds the base path before.
   CURL plItemUrl(strFilename);
   CURL plBaseUrl(strBasePath);
+  
+  if (!plItemUrl.GetProtocol().length())
+  {
+    strFilename = plBaseUrl.GetUrlWithoutOptions() + strFilename;
+    return;
+  }
+  
+  strFilename = plItemUrl.GetUrlWithoutOptions();
+  CStdString strOptions = plItemUrl.GetOptions();
+  
   int iDotDotLoc, iBeginCut, iEndCut;
 
   if (plBaseUrl.IsLocal()) //Base in local directory
@@ -676,6 +686,8 @@ void CUtil::GetQualifiedFilename(const CStdString &strBasePath, CStdString &strF
       strFilename.Delete(iBeginCut, iEndCut - iBeginCut);
     }
   }
+  
+  strFilename = strFilename + strOptions;
 }
 
 void CUtil::RunShortcut(const char* szShortcutPath)
