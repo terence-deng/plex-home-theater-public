3ca6609e3bc7950c02f73e8fbd5e9ab30cf14832
Elan Feingold <elan@plexapp.com>
2011-08-03 19:51:33 -1000

    Check for the right Now Playing string.

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -4466,7 +4466,7 @@ void CApplication::ActivateVisualizer()
 {
   // See which visualizer to activate.
   CStdString name = g_guiSettings.GetString("musicplayer.visualisation");
-  if (name == "Now Playing.vis" || name == "visualization.nowplaying")
+  if (name == "Now Playing" || name == "visualization.nowplaying")
     g_windowManager.ActivateWindow(WINDOW_NOW_PLAYING);
   else
     g_windowManager.ActivateWindow(WINDOW_VISUALISATION);
