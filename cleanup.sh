f230438b573228c1ce2dd4ce81e5a1e8fe15adc4
Elan Feingold <elan@plexapp.com>
2011-10-19 12:43:45 -1000

    Tweak release scripts.

--- cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-pushd build/Release/Plex.app/Contents/Resources/Plex
-rm -rf addons/metadata.*
-rm -rf addons/skin.mediastream/.git
-rm -rf addons/skin.confluence
-cd addons/skin.mediastream
-../../../../../../../../tools/TexturePacker/TexturePacker -input media
-rm media/*
-mv Textures.xbt media/
--- prep-release.sh
+++ prep-release.sh
@@ -1,9 +1,11 @@
-pushd build/Release/Plex.app/Contents/Resources/Plex/addons
-rm -rf skin.confluence
-cd skin.mediastream
+#!/bin/sh
+pushd build/Release/Plex.app/Contents/Resources/Plex
+rm -rf addons/metadata.*
+rm -rf addons/skin.mediastream/.git
+rm -rf addons/skin.confluence
+cd addons/skin.mediastream
 ../../../../../../../../tools/TexturePacker/TexturePacker -input media
 rm media/*
 rm -rf media-lite
-mv Textures.xbt media
+mv Textures.xbt media/
 rm -rf .git
-popd
