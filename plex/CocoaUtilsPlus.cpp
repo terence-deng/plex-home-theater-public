07f67dde0e2523bb716cc2034f89bec2020012b9
Elan Feingold <elan@plexapp.com>
2011-10-21 12:11:20 -1000

    Moved the cross-platform utilities code over to the cross-platform utilities file.

--- plex/CocoaUtilsPlus.cpp
+++ plex/CocoaUtilsPlus.cpp
@@ -6,20 +6,67 @@
 //  Copyright 2011 Plex Inc. All rights reserved.
 //
 
+#ifdef __APPLE__
+#include <CoreServices/CoreServices.h>
+#endif
+
+#include <boost/lexical_cast.hpp>
 #include "CocoaUtilsPlus.h"
 
-#ifdef _WIN32
-
-// TODO - mfeingol - Implement these correctly
-
+///////////////////////////////////////////////////////////////////////////////
 string Cocoa_GetMachinePlatform()
 {
-  return "Windows";
+  string platform;
+  
+#if defined(_WIN32)
+  platform = "Windows";
+#elif defined(__linux__)
+  platform = "Linux";
+#else
+  platform = "MacOSX";
+#endif
+  
+  return platform;
 }
 
+/////////////////////////////////////////////////////////////////////////////
 string Cocoa_GetMachinePlatformVersion()
 {
-  return "6.1";
-}
-
+  string ver;
+  
+#if defined(_WIN32)
+  
+  DWORD dwVersion = GetVersion();
+  DWORD dwMajorVersion = (DWORD)(LOBYTE(LOWORD(dwVersion)));
+  DWORD dwMinorVersion = (DWORD)(HIBYTE(LOWORD(dwVersion)));
+  DWORD dwBuildNumber  = (DWORD)(HIWORD(dwVersion));
+  
+  char str[256];
+  sprintf(str, "%d.%d (Build %d)", dwMajorVersion, dwMinorVersion, dwBuildNumber);
+  ver = str;
+  
+#elif defined(__linux__)
+  
+  struct utsname buf;
+  if (uname(&buf) == 0)
+  {
+    ver = buf.release;
+    ver = " (" + string(buf.version) + ")";
+  }
+  
+#else
+  
+  SInt32 res = 0; 
+  Gestalt(gestaltSystemVersionMajor, &res);
+  ver = boost::lexical_cast<string>(res) + ".";
+  
+  Gestalt(gestaltSystemVersionMinor, &res);
+  ver += boost::lexical_cast<string>(res) + ".";
+  
+  Gestalt(gestaltSystemVersionBugFix, &res);
+  ver += boost::lexical_cast<string>(res);
+  
 #endif
+  
+  return ver;
+}
