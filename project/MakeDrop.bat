5575bfd19845e499f5ad17b397460193b319ea21
mfeingol <maf6@cornell.edu>
2011-10-24 10:26:44 -0700

    Another makedrop cleanup

--- project/MakeDrop.bat
+++ project/MakeDrop.bat
@@ -72,32 +72,29 @@ rem ************************************
 echo Copying static files into deployment directory...
 rem ************************************
 
-copy "%PlexRoot%\copying.txt" "%DeployDir%" > NUL
-copy "%PlexRoot%\known_issues.txt" "%DeployDir%" > NUL
-copy "%PlexRoot%\LICENSE.GPL" "%DeployDir%" > NUL
-copy "%PlexRoot%\Plex\Windows\winsparkle\WinSparkle-0.3\WinSparkle.dll" "%DeployDir%" > NUL
-xcopy "%PlexRoot%\project\Win32BuildSetup\dependencies\*.*" "%DeployDir%" /Q /I /Y /EXCLUDE:exclude.txt  > NUL
-rem xcopy "%PlexRoot%\project\Win32BuildSetup\vs_redistributable\vs2010\vcredist_x86.exe" "%DeployDir%" /Q /I /Y /EXCLUDE:exclude.txt  > NUL
-xcopy "%PlexRoot%\userdata" "%DeployDir%\userdata" /E /Q /I /Y /EXCLUDE:exclude.txt > NUL
+copy "%PlexRoot%\copying.txt" "%DeployDir%"
+
+rem This file doesn't appear to exist at the moment, so make it optional
+if exist "%PlexRoot%\known_issues.txt" copy "%PlexRoot%\known_issues.txt" "%DeployDir%"
+
+copy "%PlexRoot%\LICENSE.GPL" "%DeployDir%"
+copy "%PlexRoot%\Plex\Windows\winsparkle\WinSparkle-0.3\WinSparkle.dll" "%DeployDir%"
+xcopy "%PlexRoot%\project\Win32BuildSetup\dependencies\*.*" "%DeployDir%" /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "%PlexRoot%\userdata" "%DeployDir%\userdata" /E /Q /I /Y /EXCLUDE:exclude.txt
   
-xcopy "%PlexRoot%\language" "%DeployDir%\language" /E /Q /I /Y /EXCLUDE:exclude.txt  > NUL
-xcopy "%PlexRoot%\addons" "%DeployDir%\addons" /E /Q /I /Y /EXCLUDE:exclude.txt > NUL
-xcopy "%PlexRoot%\system" "%DeployDir%\system" /E /Q /I /Y /EXCLUDE:exclude.txt  > NUL
-xcopy "%PlexRoot%\media" "%DeployDir%\media" /E /Q /I /Y /EXCLUDE:exclude.txt  > NUL
-xcopy "%PlexRoot%\sounds" "%DeployDir%\sounds" /E /Q /I /Y /EXCLUDE:exclude.txt > NUL
+xcopy "%PlexRoot%\language" "%DeployDir%\language" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "%PlexRoot%\addons" "%DeployDir%\addons" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "%PlexRoot%\system" "%DeployDir%\system" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "%PlexRoot%\media" "%DeployDir%\media" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "%PlexRoot%\sounds" "%DeployDir%\sounds" /E /Q /I /Y /EXCLUDE:exclude.txt
 
-if exist exclude.txt del exclude.txt
+if exist exclude.txt del /f exclude.txt
 
 rem ************************************
-echo Building MediaStream Skin...
-rem ************************************
-
-REM SET build_path=%CD%
-REM cd "%PlexRoot%\addons\skin.mediastream"
-REM call build.bat > NUL
-REM cd %build_path%
-
+echo Building MediaStream skin...
 echo Creating MediaStream build folder...
+rem ************************************
+
 set MediaStreamSourceDir=%PlexRoot%\addons\skin.mediastream
 set MediaStreamDeployDir=%DeployDir%\addons\skin.mediastream
 
@@ -106,6 +103,11 @@ if not exist "%MediaStreamDeployDir%\Media" md "%MediaStreamDeployDir%\Media"
 echo Creating XBT texture files...
 echo Running command "%PlexRoot%\Tools\TexturePacker\TexturePacker.exe" -input "%MediaStreamSourceDir%\Media" -output "%MediaStreamDeployDir%\Media\Textures.xbt"
 start "" /b /wait "%PlexRoot%\Tools\TexturePacker\TexturePacker.exe" -input "%MediaStreamSourceDir%\Media" -output "%MediaStreamDeployDir%\Media\Textures.xbt"
+
+if not exist "%MediaStreamDeployDir%\Media\Textures.xbt" (
+  echo FATAL ERROR - Could not create Textures.xbt!
+  goto :End
+)
 echo XBT texture files created...
 
 echo ------------------------------------------------------------
