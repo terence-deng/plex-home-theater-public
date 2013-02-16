276d3ce27de772cb35951aecb684b1d6491a1b2a
Dale Hamel <dale.hamel@srvthe.net>
2013-02-16 08:57:02 -0600

    Added openelec stuff from weelkin's changes

--- plex/CMakeModules/CMakeConfigRPi.cmake
+++ plex/CMakeModules/CMakeConfigRPi.cmake
@@ -137,32 +137,38 @@ add_definitions(
     -DNDEBUG=1 
 )
 
-include_directories(
-    /opt/vc/include/ 
-    /opt/vc/include/EGL 
-   # /opt/vc/include/GLES 
-    /opt/vc/include/GLES2 
-    /opt/vc/include/KHR 
-    /opt/vc/include/VG 
-    /opt/vc/include/WF 
-    /opt/vc/include/vc/include  
-)
+#include_directories(
+#    /opt/vc/include/ 
+#    /opt/vc/include/EGL 
+#   # /opt/vc/include/GLES 
+#    /opt/vc/include/GLES2 
+#    /opt/vc/include/KHR 
+#    /opt/vc/include/VG 
+#    /opt/vc/include/WF 
+#    /opt/vc/include/vc/include  
+#)
 
 
 
 
 
-plex_find_library(GLESv2 0  0 /opt/vc/lib 1)
-plex_find_library(EGL 0 0  /opt/vc/lib 1)
-plex_find_library(vcos 0 0  /opt/vc/lib 1)
-plex_find_library(bcm_host 0 0  /opt/vc/lib 1)
-plex_find_library(vchiq_arm 0 0  /opt/vc/lib 1)
+plex_find_library(GLESv2 0  0 system/opt/vc/lib 1)
+plex_find_library(EGL 0 0  system/opt/vc/lib 1)
+plex_find_library(vcos 0 0  system/opt/vc/lib 1)
+plex_find_library(bcm_host 0 0  system/opt/vc/lib 1)
+plex_find_library(vchiq_arm 0 0  system/opt/vc/lib 1)
 
 
-set(CMAKE_C_FLAGS " -isystem/usr/include -isystem/opt/vc/include -isystem/opt/vc/include/interface/vcos/pthreads -isystem/opt/vc -isystem/opt/vc/include/interface/vmcs_host/linux/ -isystem/opt/vc/include/EGL -isystem/opt/vc/include/GLES -isystem/opt/vc/include/GLES2 -isystem/opt/vc/include/KHR -isystem/opt/vc/include/VG -L/lib -L/usr/lib -L/opt/vc/lib -Wl,-rpath-link,/lib -Wl,-rpath-link,/lib -Wl,-rpath-link,/usr/lib -Wl,-rpath-link,/opt/vc/ -fPIC -pipe -mcpu=arm1176jzf-s -mtune=arm1176jzf-s -mfloat-abi=hard -mfpu=vfp -mabi=aapcs-linux -Wno-psabi -Wa,-mno-warn-deprecated -Wno-deprecated-declarations -g -pg")
+#needed for the commandline flag CMAKE_INCLUDE_PATH
+foreach(path ${CMAKE_INCLUDE_PATH})
+    include_directories(${path})
+endforeach()
 
-set(CMAKE_CXX_FLAGS ${CMAKE_C_FLAGS})
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -g -pg")
+
+#set(CMAKE_C_FLAGS " -isystem/usr/include -isystem/opt/vc/include -isystem/opt/vc/include/interface/vcos/pthreads -isystem/opt/vc -isystem/opt/vc/include/interface/vmcs_host/linux/ -isystem/opt/vc/include/EGL -isystem/opt/vc/include/GLES -isystem/opt/vc/include/GLES2 -isystem/opt/vc/include/KHR -isystem/opt/vc/include/VG -L/lib -L/usr/lib -L/opt/vc/lib -Wl,-rpath-link,/lib -Wl,-rpath-link,/lib -Wl,-rpath-link,/usr/lib -Wl,-rpath-link,/opt/vc/ -fPIC -pipe -mcpu=arm1176jzf-s -mtune=arm1176jzf-s -mfloat-abi=hard -mfpu=vfp -mabi=aapcs-linux -Wno-psabi -Wa,-mno-warn-deprecated -Wno-deprecated-declarations -g -pg")
+
+#set(CMAKE_CXX_FLAGS ${CMAKE_C_FLAGS})
+#set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -g -pg")
 
 message (STATUS "Enabling profiling with gprof")
 set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -pg")
@@ -176,7 +182,7 @@ set(RESOURCEPATH share/XBMC)
 
 set(PLEX_LINK_WRAPPED "-Wl,--unresolved-symbols=ignore-all -Wl,-wrap,_IO_getc -Wl,-wrap,_IO_getc_unlocked -Wl,-wrap,_IO_putc -Wl,-wrap,__fgets_chk -Wl,-wrap,__fprintf_chk -Wl,-wrap,__fread_chk -Wl,-wrap,__fxstat64 -Wl,-wrap,__lxstat64 -Wl,-wrap,__printf_chk -Wl,-wrap,__read_chk -Wl,-wrap,__vfprintf_chk -Wl,-wrap,__xstat64 -Wl,-wrap,_stat -Wl,-wrap,calloc -Wl,-wrap,clearerr -Wl,-wrap,close -Wl,-wrap,closedir -Wl,-wrap,dlopen -Wl,-wrap,fclose -Wl,-wrap,fdopen -Wl,-wrap,feof -Wl,-wrap,ferror -Wl,-wrap,fflush -Wl,-wrap,fgetc -Wl,-wrap,fgetpos -Wl,-wrap,fgetpos64 -Wl,-wrap,fgets -Wl,-wrap,fileno -Wl,-wrap,flockfile -Wl,-wrap,fopen -Wl,-wrap,fopen64 -Wl,-wrap,fprintf -Wl,-wrap,fputc -Wl,-wrap,fputs -Wl,-wrap,fread -Wl,-wrap,free -Wl,-wrap,freopen -Wl,-wrap,fseek -Wl,-wrap,fseeko64 -Wl,-wrap,fsetpos -Wl,-wrap,fsetpos64 -Wl,-wrap,fstat -Wl,-wrap,ftell -Wl,-wrap,ftello64 -Wl,-wrap,ftrylockfile -Wl,-wrap,funlockfile -Wl,-wrap,fwrite -Wl,-wrap,getc -Wl,-wrap,getc_unlocked -Wl,-wrap,getmntent -Wl,-wrap,ioctl -Wl,-wrap,lseek -Wl,-wrap,lseek64 -Wl,-wrap,malloc -Wl,-wrap,open -Wl,-wrap,open64 -Wl,-wrap,opendir -Wl,-wrap,popen -Wl,-wrap,printf -Wl,-wrap,read -Wl,-wrap,readdir -Wl,-wrap,readdir64 -Wl,-wrap,realloc -Wl,-wrap,rewind -Wl,-wrap,rewinddir -Wl,-wrap,setvbuf -Wl,-wrap,ungetc -Wl,-wrap,vfprintf -Wl,-wrap,write")
 
-set(CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH} /opt/vc/lib )
+set(CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH} system/opt/vc/lib )
 
 
 set(PLEX_LINK_WHOLEARCHIVE -Wl,--whole-archive)
