dfac2a739ddab5731f8c875c8fa47de90d5cd44d
LongChair <LongChair@hotmail.com>
2014-02-22 11:32:33 +0100

    Better Fix For theme Music

--- plex/PlexThemeMusicPlayer.cpp
+++ plex/PlexThemeMusicPlayer.cpp
@@ -26,8 +26,6 @@ void CPlexThemeMusicPlayer::initPlayer()
   {
     m_player = CPlayerCoreFactory::CreatePlayer(EPC_PAPLAYER, *this);
     m_player->RegisterAudioCallback(this);
-    m_player->SetVolume(g_guiSettings.GetInt("backgroundmusic.bgmusicvolume") / 100.0);
-    m_player->FadeOut(2 * 1000);
   }
 }
 
@@ -97,12 +95,8 @@ void CPlexThemeMusicPlayer::OnJobComplete(unsigned int jobID, bool success, CJob
 
       initPlayer();
       m_player->OpenFile(*m_currentItem.get(), CPlayerOptions());
-
-    #ifdef TARGET_RASPBERRY_PI
-      // fixes theme music playback on RasPI. Seems that volume is reseted at least with OMX player when creating the OMX objects
       m_player->SetVolume(g_guiSettings.GetInt("backgroundmusic.bgmusicvolume") / 100.0);
       m_player->FadeOut(2 * 1000);
-    #endif
     }
   }
   else if (m_player && m_player->IsPlaying() && !m_player->IsPaused())
